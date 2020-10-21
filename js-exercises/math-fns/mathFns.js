7448ce87aadd28aa34a988d1b1ddd675394ccad5
Ashim Raj Konwar <ashimrajkonwar@gmail.com>
2020-10-22 03:56:57 +0530

    INIT + MathFxn Sol

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,12 @@
+// You can use the function from the `Math` module.
+const sqrt = (n) => Math.sqrt(n);
+
+const power = (x, y) => x ** y;
+
+const round = (x) => Math.round(x);
+// Don't change the exported names.
+export {
+  sqrt,
+  power,
+  round,
+};
