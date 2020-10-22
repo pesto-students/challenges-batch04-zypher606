f141607ed5e5f1b8ebbf873e975f012000a261cc
Ashim Raj Konwar <ashimrajkonwar@gmail.com>
2020-10-23 03:11:59 +0530

    Solve rot-13 problem statement

--- js-exercises/rot-13/rot13.js
+++ js-exercises/rot-13/rot13.js
@@ -1,5 +1,18 @@
 function rot13(...args) {
-  return args;
+  const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const cypheredMap = {};
+
+  for (const char of alphabets) {
+    cypheredMap[char] = alphabets[(alphabets.indexOf(char) + 13) % 26];
+  }
+
+  const input = [...args[0]];
+  for (let i = 0; i < input.length; i += 1) {
+    if (cypheredMap[input[i]]) {
+      input[i] = cypheredMap[input[i]];
+    }
+  }
+  return input.join('');
 }
 
 export {
