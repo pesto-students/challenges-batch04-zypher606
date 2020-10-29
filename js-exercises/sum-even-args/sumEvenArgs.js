969ddc1bf07cc6262ebba11995054d224deecd83
zypher606 <ashimrajkonwar@gmail.com>
2020-10-29 11:02:16 +0530

    Add solution ton sum-even-args

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,6 +1,6 @@
 const sumEvenArgs = (...args) => {
-  console.log(args);
-  return args;
+  const evenItems = args.filter(item => item % 2 === 0);
+  return evenItems.reduce((a, b) => a + b, 0);
 };
 
 export { sumEvenArgs };
