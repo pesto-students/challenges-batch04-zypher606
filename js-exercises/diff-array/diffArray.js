a151686c768c833f3ab02f2895e37d86c5c6f784
zypher606 <ashimrajkonwar@gmail.com>
2020-10-29 11:25:29 +0530

    Add solution to diff-array

--- js-exercises/diff-array/diffArray.js
+++ js-exercises/diff-array/diffArray.js
@@ -1,5 +1,16 @@
-function diffArray() {
-  return null;
+function diffArray(array1, array2) {
+  const frequencyMap = {};
+  for (const item of array1) {
+    frequencyMap[item] = 1;
+  }
+
+  for (const item of array2) {
+    if (frequencyMap[item]) frequencyMap[item] += 1;
+    else frequencyMap[item] = 1;
+  }
+
+  const uniqueItems = [...array1, ...array2].filter(item => frequencyMap[item] === 1);
+  return uniqueItems;
 }
 
 export {
