a8c371a0231c317ace698c54b61f33b365dd587b
zypher606 <ashimrajkonwar@gmail.com>
2020-10-31 00:11:31 +0530

    Add solution to balanced-braces problem

--- js-exercises/balanced-braces/balancedBraces.js
+++ js-exercises/balanced-braces/balancedBraces.js
@@ -1,5 +1,18 @@
-function balancedBraces(...args) {
-  return args;
+function balancedBraces(expression) {
+  const stack = [];
+  const bracesMap = { '{': '}', '(': ')', '[': ']' };
+  const startBraces = Object.keys(bracesMap);
+  const endBraces = Object.values(bracesMap);
+  for (const char of expression) {
+    if (stack.length !== 0 && endBraces.includes(char)) {
+      const lastItem = stack[stack.length - 1];
+      if (bracesMap[lastItem] === char) stack.pop();
+    } else if (startBraces.includes(char) || endBraces.includes(char)) {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
 }
 
 export {
