d201f4dce782cf362f53974586a4a6c81fbb15e7
zypher606 <ashimrajkonwar@gmail.com>
2020-11-02 02:13:52 +0530

    Add solution to curried function problem

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,5 +1,11 @@
-function curry() {
-  return null;
+function curry(fxn) {
+  return function curryHandle(...args1) {
+    if (args1.length >= fxn.length) {
+      return fxn(...args1);
+    }
+
+    return (...args2) => curryHandle.apply(this, [...args1, ...args2]);
+  };
 }
 
 export {
