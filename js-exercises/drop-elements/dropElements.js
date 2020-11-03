c5f278c702ed9bb48d74b48b85370b189650d695
zypher606 <ashimrajkonwar@gmail.com>
2020-11-03 15:41:12 +0530

    Init drop-elements problem files

---
+++ js-exercises/drop-elements/dropElements.js
@@ -0,0 +1,5 @@
+function dropElements(elements, predicate) {
+  return [elements, predicate];
+}
+
+export { dropElements };
---
+++ js-exercises/drop-elements/dropElements.test.js
@@ -0,0 +1,11 @@
+import { dropElements } from './dropElements';
+
+describe('dropElements', () => {
+  test('should return the correct output', () => {
+    expect(dropElements([1, 2, 3, 4], (n) => n >= 3)).toEqual([3, 4]);
+    expect(dropElements([0, 1, 0, 1], (n) => n === 1)).toEqual([1, 1]);
+    expect(dropElements([1, 2, 3, 4], (n) => n > 5)).toEqual([]);
+    expect(dropElements([1, 2, 3, 7, 4], (n) => n > 3)).toEqual([7, 4]);
+    expect(dropElements([1, 2, 3, 9, 2], (n) => n > 2)).toEqual([3, 9]);
+  });
+});
