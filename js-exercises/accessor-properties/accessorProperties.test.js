c03663bdc4481f7184a7f014a343267bf3f558cd
zypher606 <ashimrajkonwar@gmail.com>
2020-11-04 15:42:56 +0530

    Add solution to object-utils problem

--- js-exercises/accessor-properties/accessorProperties.test.js
+++ js-exercises/accessor-properties/accessorProperties.test.js
@@ -1,13 +1,13 @@
 import { accessorProperties } from './accessorProperties';
 
-describe('Template Test', () => {
-  test('Template Test', () => {
+describe('Accessor properties Test', () => {
+  test('Test 1', () => {
     const obj = accessorProperties();
     obj.number = 36;
     expect(obj.number).toBe(100100);
   });
 
-  test('Template Test 2', () => {
+  test('Test 2', () => {
     const obj = accessorProperties();
     expect(obj.number).toBe(undefined);
   });
